a173611527e61b083a0ff9994903de0cc85a4674
BananILN <daniil.klovans84@mail.ru>
2025-02-03 23:20:40 +0400

    add server and dependens of npm + index.js

---
+++ easyEn/src/server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
