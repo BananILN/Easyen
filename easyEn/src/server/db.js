2129d0f51cd241c1f35d59de1564bcfbdbc21905
BananILN <daniil.klovans84@mail.ru>
2025-02-04 13:40:14 +0400

    add server and dependens of npm + index.js

---
+++ easyEn/src/server/db.js
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+import { config } from 'dotenv'; 
+
+config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Название базы данных
+  process.env.DB_USER, // Имя пользователя
+  process.env.DB_PASSWORD, // Пароль
+  {
+    dialect: 'postgres', // Используемая СУБД
+    host: process.env.DB_HOST, // Хост
+    port: process.env.DB_PORT, // Порт
+  }
+);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
+export { sequelize };
--- easyEn/src/server/index.js
+++ easyEn/src/server/index.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { sequelize } from './db.js';
+
+config(); // Загружаем переменные окружения
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+  try {
+    await sequelize.authenticate(); // Проверка подключения к базе данных
+    console.log('Connection to the database has been established successfully.');
+
+    await sequelize.sync(); // Синхронизация моделей с базой данных
+    console.log('All models were synchronized successfully.');
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+start();
