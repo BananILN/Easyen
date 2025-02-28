b23956d8d257cfb88ee0fd956df47cb828e34281
BananILN <daniil.klovans84@mail.ru>
2025-02-28 21:00:34 +0400

    get new architector

---
+++ easyEn/client/src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+import { UserContext } from "./UserContext.jsx";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) =>{
+    const [isAuth, setIsAuth] = useState(false)
+    const { updateUser } = useContext(UserContext)
+
+    const login = () => {
+        setIsAuth(true);
+        updateUser({ name: 'John Doe', email: 'john@example.com' }); 
+      };
+    
+      const logout = () => {
+        setIsAuth(false);
+        updateUser(null); 
+      };
+    return (
+        <AuthContext.Provider value={{ isAuth, login, logout }}>
+          {children}
+        </AuthContext.Provider>
+      );
+}
